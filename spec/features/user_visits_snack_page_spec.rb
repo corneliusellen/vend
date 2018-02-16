716c79773033fde659c60b838c8f9ec46d0e3d5d
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-16 10:00:39 -0700

    Write spec for snack page.

---
+++ spec/features/user_visits_snack_page_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe "when a user visits a specific snack page" do
+  it "sees name and price of snack, list of locations of vending machines that carry it, average price for snacks in those vending machines, and count of different kinds of items in those vending machines" do
+
+  end
+end
